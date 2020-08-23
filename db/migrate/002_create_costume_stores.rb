0801d98f5248637069e708439154d99b0182196d
kowalix-pl <kowalix007@gmail.com>
2020-08-23 17:15:52 +0100

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+# class CostumeStores < ActiveRecord::Migration
+#   def change
+#     create_table :costumes do |t|
+#       t.string :name
+#       t.string :location
+#       t.integer :costume_inventory
+#       t.integer :num_of_employees
+#       t.timestamp :opening_time
+#       t.timestamp :closing_time
+#       t.boolean :still_in_business
+#    end 
+#   end
+# end 
